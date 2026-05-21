4c981578d6052a4b241485097b5932cf72e3cade
Chetan Kaurav <chetankaurav9@gmail.com>
2026-05-21 13:27:28 +0530

    initial commet

---
+++ models/unet_model.py
@@ -0,0 +1,17 @@
+import segmentation_models_pytorch as smp
+
+
+def build_unet():
+
+    model = smp.Unet(
+
+        encoder_name="resnet34",
+        encoder_weights="imagenet",
+
+        in_channels=1,
+        classes=1,
+
+        activation=None
+    )
+
+    return model
